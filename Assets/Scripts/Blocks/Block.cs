89967d4aaabb7cb622fb864eb7361270bc0ff1df
mrSaliman <sloi.25abc@gmail.com>
2023-10-07 12:10:11 +0300

    throw zones done.

--- Assets/Scripts/Blocks/Block.cs
+++ Assets/Scripts/Blocks/Block.cs
@@ -1,30 +1,30 @@
 ﻿using Physics;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Blocks
 {
     public abstract class Block : MonoBehaviour
     {
-        private PhysicsObject2D _physicsObject;
+        [SerializeReference]
+        private PhysicsObject2D physicsObject;
 
         public BlockData blockData;
-        private SpriteRenderer _spriteRenderer;
+        [SerializeReference]
+        private SpriteRenderer spriteRenderer;
         
-        private Camera _mainCamera;
+        [SerializeReference]
+        private Camera mainCamera;
         private float _cameraHeight;
         private float _cameraWidth;
         
         private void Awake()
         {
-            _physicsObject = GetComponent<PhysicsObject2D>();
-            _spriteRenderer = GetComponent<SpriteRenderer>();
-            
             SetSprite();
             
-            _mainCamera = Camera.main;
-            _cameraHeight = 2f * _mainCamera.orthographicSize;
-            _cameraWidth = _cameraHeight * _mainCamera.aspect;
+            _cameraHeight = 2f * mainCamera.orthographicSize;
+            _cameraWidth = _cameraHeight * mainCamera.aspect;
         }
 
         public abstract void OnHit();
@@ -33,28 +33,24 @@ namespace Blocks
 
         private void Update()
         {
-            if (!(_physicsObject.velocity.y < 0) ||
-                (!(transform.position.y + blockData.radius < -_cameraHeight / 2f - 1f) &&
-                 !(transform.position.x + blockData.radius < -_cameraWidth / 2f - 1f) &&
-                 !(transform.position.x - blockData.radius > _cameraWidth / 2f + 1f))) return;
+            if (!(physicsObject.velocity.y < 0) ||
+                (!(transform.position.y + blockData.radius < -_cameraHeight / 2f) &&
+                 !(transform.position.x + blockData.radius < -_cameraWidth / 2f) &&
+                 !(transform.position.x - blockData.radius > _cameraWidth / 2f))) return;
             Destroy(gameObject);
             //OnMiss();
         }
 
         private void SetSprite()
         {
-            _spriteRenderer.sprite = blockData.sprite;
+            spriteRenderer.sprite = blockData.sprite;
         }
 
-        public void ThrowItself(Vector3 position, float angle, float velocity, float angularVelocity)
+        public void ThrowItself(Vector3 position, Vector2 velocity, float angularVelocity)
         {
             transform.position = position;
-            _physicsObject.SetMainAndAngularVelocity(
-                new Vector2(
-                    velocity * math.cos(angle), 
-                    velocity * math.sin(angle)), 
-                angularVelocity);
-            _physicsObject.isFrozen = false;
+            physicsObject.SetMainAndAngularVelocity(velocity, angularVelocity);
+            physicsObject.isFrozen = false;
         }
     }
 }
